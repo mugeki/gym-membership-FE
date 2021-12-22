e70e83928b2118091d0c9d3acdeede71dd12e65b
Muhamad Elang Ramadhan <elangmugeki@gmail.com>
2021-12-22 17:15:36 +0700

    implement form validation

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,17 +1,27 @@
 import Link from "next/link";
 import { useState } from "react";
 import { FloatingLabel, Form } from "react-bootstrap";
+import useValidateForm from "../../hooks/useValidateForm";
 import styles from "../../styles/UserAuth.module.css";
 
 export default function Login() {
+	const { validateForm } = useValidateForm();
 	const [form, setForm] = useState({
 		username: "",
 		password: "",
 	});
+	const [errorMsg, setErrorMsg] = useState({});
 	const onChange = (e) => {
 		const name = e.target.name;
 		const value = e.target.value;
 		setForm({ ...form, [name]: value });
+	};
+	const onSubmit = (e) => {
+		e.preventDefault();
+		const newErrors = validateForm(undefined, undefined, form);
+		if (Object.keys(newErrors).length > 0) {
+			setErrorMsg(newErrors);
+		}
 	};
 	return (
 		<div
@@ -22,35 +32,43 @@ export default function Login() {
 				<h1 className="fw-bolder">THATS FIT FOR U.</h1>
 				<h1 className="fw-bolder">WHOEVER U ARE!</h1>
 			</div>
-			<form className="container flex-grow d-flex flex-column mb-5">
-				<FloatingLabel
-					className="text-white mb-4 border-bottom border-secondary"
-					label="Username"
-				>
+			<Form
+				className="container flex-grow d-flex flex-column mb-5"
+				noValidate
+				onSubmit={onSubmit}
+			>
+				<FloatingLabel className="text-light mb-4" label="Username">
 					<Form.Control
-						className={`${styles.input} rounded-0 border-0 shadow-none text-white bg-transparent`}
+						className={`${styles.input} rounded-0 border-0 border-bottom border-secondary shadow-none text-white bg-transparent`}
 						type="text"
 						placeholder=" "
 						name="username"
 						value={form.username}
 						onChange={onChange}
+						isInvalid={!!errorMsg.username}
 					/>
+					<Form.Control.Feedback type="invalid">
+						{errorMsg.username}
+					</Form.Control.Feedback>
 				</FloatingLabel>
-				<FloatingLabel
-					className="text-white mb-4 border-bottom border-secondary"
-					label="Password"
-				>
+				<FloatingLabel className="text-light mb-4" label="Password">
 					<Form.Control
-						className={`${styles.input} rounded-0 border-0 shadow-none text-white bg-transparent`}
+						className={`${styles.input} rounded-0 border-0 border-bottom border-secondary shadow-none text-white bg-transparent`}
 						type="password"
 						placeholder=" "
 						name="password"
 						value={form.password}
 						onChange={onChange}
+						isInvalid={!!errorMsg.password}
 					/>
+					<Form.Control.Feedback type="invalid">
+						{errorMsg.password}
+					</Form.Control.Feedback>
 				</FloatingLabel>
-				<button className="btn btn-secondary text-white mb-5">Login</button>
-			</form>
+				<button className="btn btn-secondary text-white mb-5" type="submit">
+					Login
+				</button>
+			</Form>
 			<div className="flex-grow d-flex justify-content-between pb-3 px-4">
 				<Link href="register">
 					<a className="text-secondary m-0 text-decoration-none">Sign Up</a>
--- pages/register/index.js
+++ pages/register/index.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import { useState } from "react";
 import { FloatingLabel, Form } from "react-bootstrap";
+import useValidateForm from "../../hooks/useValidateForm";
 import styles from "../../styles/UserAuth.module.css";
 
 export default function Register() {
+	const { validateForm } = useValidateForm();
 	const [form, setForm] = useState({
 		username: "",
 		email: "",
@@ -12,10 +14,24 @@ export default function Register() {
 		telephone: "",
 		gender: "",
 	});
+	const [errorMsg, setErrorMsg] = useState({});
 	const onChange = (e) => {
 		const name = e.target.name;
 		const value = e.target.value;
 		setForm({ ...form, [name]: value });
+	};
+	const onBlur = (e) => {
+		const name = e.target.name;
+		const value = e.target.value;
+		const messages = validateForm(name, value);
+		setErrorMsg({ ...errorMsg, ...messages });
+	};
+	const onSubmit = (e) => {
+		e.preventDefault();
+		const newErrors = validateForm(undefined, undefined, form);
+		if (Object.keys(newErrors).length > 0) {
+			setErrorMsg(newErrors);
+		}
 	};
 	return (
 		<div
@@ -26,92 +42,103 @@ export default function Register() {
 				<h1 className="fw-bolder">THATS FIT FOR U.</h1>
 				<h1 className="fw-bolder">WHOEVER U ARE!</h1>
 			</div>
-			<form className="container flex-grow d-flex flex-column mb-5">
-				<FloatingLabel
-					className="text-white mb-4 border-bottom border-secondary"
-					label="Username"
-				>
+			<Form
+				className="container flex-grow d-flex flex-column mb-5"
+				noValidate
+				onSubmit={onSubmit}
+			>
+				<FloatingLabel className="text-light mb-4" label="Username">
 					<Form.Control
-						className={`${styles.input} rounded-0 border-0 shadow-none text-white bg-transparent`}
+						className={`${styles.input} rounded-0 border-0 border-bottom border-secondary shadow-none text-white bg-transparent`}
 						type="text"
 						placeholder=" "
 						name="username"
 						value={form.username}
 						onChange={onChange}
+						onBlur={onBlur}
+						isInvalid={!!errorMsg.username}
+						required
 					/>
+					<Form.Control.Feedback type="invalid">
+						{errorMsg.username}
+					</Form.Control.Feedback>
 				</FloatingLabel>
-				<FloatingLabel
-					className="text-white mb-4 border-bottom border-secondary"
-					label="Email"
-				>
+				<FloatingLabel className="text-light mb-4" label="Email">
 					<Form.Control
-						className={`${styles.input} rounded-0 border-0 shadow-none text-white bg-transparent`}
+						className={`${styles.input} rounded-0 border-0 border-bottom border-secondary shadow-none text-white bg-transparent`}
 						type="email"
 						placeholder=" "
 						name="email"
 						value={form.email}
 						onChange={onChange}
+						onBlur={onBlur}
+						isInvalid={!!errorMsg.email}
+						required
 					/>
+					<Form.Control.Feedback type="invalid">
+						{errorMsg.email}
+					</Form.Control.Feedback>
 				</FloatingLabel>
-				<FloatingLabel
-					className="text-white mb-4 border-bottom border-secondary"
-					label="Full Name"
-				>
+				<FloatingLabel className="text-light mb-4" label="Full Name">
 					<Form.Control
-						className={`${styles.input} rounded-0 border-0 shadow-none text-white bg-transparent`}
+						className={`${styles.input} rounded-0 border-0 border-bottom border-secondary shadow-none text-white bg-transparent`}
 						type="text"
 						placeholder=" "
 						name="full_name"
 						value={form.full_name}
 						onChange={onChange}
+						onBlur={onBlur}
+						isInvalid={!!errorMsg.full_name}
+						required
 					/>
 				</FloatingLabel>
-				<FloatingLabel
-					className="text-white mb-4 border-bottom border-secondary"
-					label="Password"
-				>
+				<FloatingLabel className="text-light mb-4" label="Password">
 					<Form.Control
-						className={`${styles.input} rounded-0 border-0 shadow-none text-white bg-transparent`}
+						className={`${styles.input} rounded-0 border-0 border-bottom border-secondary shadow-none text-white bg-transparent`}
 						type="password"
 						placeholder=" "
 						name="password"
 						value={form.password}
 						onChange={onChange}
+						onBlur={onBlur}
+						isInvalid={!!errorMsg.password}
+						required
 					/>
+					<Form.Control.Feedback type="invalid">
+						{errorMsg.password}
+					</Form.Control.Feedback>
 				</FloatingLabel>
-				<FloatingLabel
-					className="text-white mb-4 border-bottom border-secondary"
-					label="Phone Number"
-				>
+				<FloatingLabel className="text-light mb-4" label="Phone Number">
 					<Form.Control
-						className={`${styles.input} rounded-0 border-0 shadow-none text-white bg-transparent`}
+						className={`${styles.input} rounded-0 border-0 border-bottom border-secondary shadow-none text-white bg-transparent`}
 						type="tel"
 						placeholder=" "
 						name="telephone"
 						value={form.telephone}
 						onChange={onChange}
+						onBlur={onBlur}
+						isInvalid={!!errorMsg.telephone}
+						required
 					/>
+					<Form.Control.Feedback type="invalid">
+						{errorMsg.telephone}
+					</Form.Control.Feedback>
 				</FloatingLabel>
-				<FloatingLabel
-					className="text-white mb-4 border-bottom border-secondary"
-					label="Gender"
-				>
+				<FloatingLabel className="text-light mb-4" label="Gender">
 					<Form.Select
-						className={`${styles.input} rounded-0 border-0 shadow-none text-white bg-transparent`}
+						className={`${styles.input} rounded-0 border-0 border-bottom border-secondary shadow-none text-white bg-transparent`}
 						name="gender"
 						value={form.gender}
 						onChange={onChange}
 					>
-						<option className="" value="male">
-							Male
-						</option>
-						<option className="" value="female">
-							Female
-						</option>
+						<option value="male">Male</option>
+						<option value="female">Female</option>
 					</Form.Select>
 				</FloatingLabel>
-				<button className="btn btn-secondary text-white mb-5">Register</button>
-			</form>
+				<button className="btn btn-secondary text-white mb-5" type="submit">
+					Register
+				</button>
+			</Form>
 			<div className="flex-grow d-flex justify-content-between pb-3 px-4">
 				<Link href="login">
 					<a className="text-secondary m-0 text-decoration-none">Login</a>
