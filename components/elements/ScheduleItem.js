dee44ff3de35831b77f481f586ab408de0c1017e
Muhamad Elang Ramadhan <elangmugeki@gmail.com>
2021-12-28 11:54:01 +0700

    update classes json

--- components/elements/ScheduleItem.js
+++ components/elements/ScheduleItem.js
@@ -7,6 +7,8 @@ export default function ScheduleItem({ entries }) {
 	const { formatDatetime } = useFormatDatetime();
 	const dateStart = formatDatetime(entries.date_start);
 	const dateEnd = formatDatetime(entries.date_end);
+	const regexUrl =
+		/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
 	return (
 		<div className="d-flex justify-content-between align-items-center border-0 border-bottom p-4 py-3">
 			<div className="d-flex flex-column">
@@ -19,14 +21,17 @@ export default function ScheduleItem({ entries }) {
 					{dateStart.hours}:{dateStart.minutes} {dateStart.period} -{" "}
 					{dateEnd.hours}:{dateEnd.minutes} {dateEnd.period}
 				</p>
+				{!regexUrl.test(entries.location) && (
+					<p className="m-0 text-light">{entries.location}</p>
+				)}
 			</div>
-			{entries.meet_url && (
-				<Link href={entries.meet_url} passHref>
+			{regexUrl.test(entries.location) && (
+				<Link href={entries.location} passHref>
 					<div
 						className="d-flex flex-column align-items-center"
 						style={{ cursor: "pointer" }}
 					>
-						<Icon icon="logos:google-meet" width="40" />
+						<Icon icon="logos:google-meet" height="40" width="40" />
 						<p className="m-0 text-light" style={{ fontSize: "14px" }}>
 							Join Meet
 						</p>
