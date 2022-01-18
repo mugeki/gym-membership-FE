2c13d03a1513f4ab5d0ce501d4feebaa576d12c7
amirohqurrota <qurrotaayunamiroh@gmail.com>
2022-01-18 07:05:01 +0700

    page receipt

--- components/elements/TransactionItem.js
+++ components/elements/TransactionItem.js
@@ -1,20 +1,36 @@
 import Link from "next/link";
 import styles from "../../styles/Transaction.module.css"
+import React, { useState } from 'react';
 
 export default function ScheduleItem({ entries }) {
 	const date = new Date(entries.created_at);
 	const transactionData =JSON.stringify(entries);
-	const transactionID = entries.id;
 	const href = `/profile/transaction/${entries.id}`
+	const transactionID = entries.id
+	const transactionID = entries.status
 	const linkStatus=()=>{
 		if(entries.status=="accepted"){
 			return "see receipt"
 		}else{
 			if(entries.status=="waiting-for-confirmation"){
+				// setStatusColor("red")
 				return "see status"
 			}
 			else{
+				// setStatusColor("red")
 				return "pay now"
+			}
+		}
+	}
+	const statusButton=()=>{
+		if(entries.status=="accepted"){
+			return ` bg-light ${styles.containerGreen} rounded-3 px-2 m-0`
+		}else{
+			if(entries.status=="waiting-for-confirmation"){
+				return ` bg-light ${styles.containerRed} rounded-3 px-2 m-0`
+			}
+			else{
+				return ` bg-light ${styles.containerRed} rounded-3 px-2 m-0`
 			}
 		}
 	}
@@ -25,8 +41,8 @@ export default function ScheduleItem({ entries }) {
 			href={{
 				pathname: href,
 				query: {
-					// transactionID: transactionID,
-					// transactionData:transactionData
+					ID: transactionID,
+					transactionData:transactionData
 				},
 			}}
 			as={`/profile/transactions/${entries.id}`}
@@ -35,17 +51,23 @@ export default function ScheduleItem({ entries }) {
 			params
 			>
 		
-			<div className="d-flex justify-content-between align-items-center border-0 border-bottom p-4 py-3 shadow my-3 bg-body rounded">
+			<div className={` d-flex justify-content-between align-items-center border-0 border-bottom p-4 py-3 shadow my-3 bg-body rounded`}>
 				<div className="d-flex flex-column">
-					<p className="mb-2 fw-bolder text-primary">{entries.product}</p>
-					<p className="mb-0">{entries.status}</p>
+					<p className="mb-2 fw-bolder text-primary text-capitalize m-0 p-0">{entries.class_name}</p>
+					<div className={statusButton()}>
+						<p className="mb-0">{entries.status}</p>
+					</div>
+					
 					<p className="mb-0">
-						Rp {entries.price.toLocaleString().replace(/,/g, ".")}
+						Rp {entries.nominal.toLocaleString().replace(/,/g, ".")}
 					</p>
 					<p className="mb-0">Made at {date.toLocaleString("en-GB")}</p>
 				</div>
 				<div>
-					<button className={`btn ${styles.button} p-1 rounded-3`} >{linkStatus()}</button>
+					<button 
+						className={`btn ${styles.button} p-1 rounded-3`} >
+						{linkStatus()}
+					</button>
 				</div>
 			</div>
 		</Link>
--- pages/profile/transactions/[id]/index.js
+++ pages/profile/transactions/[id]/index.js
@@ -1,24 +1,29 @@
 import NavbarTop from "../../../../components/elements/NavbarTop";
 import Payment from "../../../../components/elements/Payment";
 import Receipt from "../../../../components/elements/Receipt";
+import React, { useState } from 'react';
 
-export default function TransactionByID({id, status}){
+export default function TransactionByID({ID, status,transactionData}){
     const entries={
         "id": "12235-jsh-8sub",
         "name": "1 Month Membership",
         "price": 200000,
-        "status": "waiting-for-payment",
+        "status": "accepted",
         "created_at": "2021-12-30T00:00:00.000Z"
     }
+    const [title,setTitle]=useState("")
     const productType="membership"
     const transactionDetailHandle=()=>{
 		if(entries.status=="accepted"){
+            // setTitle("Order Receipt")
 			return <Receipt entries={entries}/>
 		}else{
 			if(entries.status=="waiting-for-confirmation"){
+                // setTitle("Order Status")
 				return "see status"
 			}
 			else{
+                // setTitle("Order Payment")
 				return <Payment/>
 			}
 		}
@@ -26,7 +31,7 @@ export default function TransactionByID({id, status}){
     // const statusNav=()=>
     return(
         <>
-            <NavbarTop title={"Order Receipt"}/>
+            <NavbarTop title={title}/>
             {/* <Receipt entries={entries} productType={productType}/> */}
             <div className="p-4">
                 {transactionDetailHandle()}
