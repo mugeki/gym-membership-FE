10930762c732b2ff766439d2d75c290bd27949d4
Muhamad Elang Ramadhan <elangmugeki@gmail.com>
2022-01-25 00:12:14 +0700

    try fix ci/cd

--- components/elements/PaymentAccount.js
+++ components/elements/PaymentAccount.js
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
 import { generateAxiosConfig, handleUnauthorized } from "../../utils/helper";
-// import paymentAccountData from "../../mock_data/paymentAccount.json";
 
 export default function PaymentAccount() {
 	const router = useRouter();
